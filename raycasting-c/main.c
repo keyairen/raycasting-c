a545329d8b6ad2d738e8a2df2bcee0e66d5ec934
keyairen <179758374+keyairen@users.noreply.github.com>
2025-05-11 15:06:13 -0700

    Refactor wall code

--- raycasting-c/main.c
+++ raycasting-c/main.c
@@ -2,19 +2,19 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <SDL.h>
+#include <stdlib.h>
 #include "constants.h"
-#include "textures.h"
 #include "graphics.h"
 #include "map.h"
-#include "ray.h"
 #include "player.h"
+#include "ray.h"
+#include "wall.h"
+#include "textures.h"
 
 bool isGameRunning = false;
 int ticksLastFrame = 0;
 
 void setup(void) {
-
-	// asks upng library to decode all png files and load the wall textures array
 	loadWallTextures();
 }
 
@@ -84,56 +84,6 @@ void update(void) {
 	castAllRays();
 }
 
-void renderWallProjection(void) {
-	for (int x = 0; x < NUM_RAYS; x++) {
-		float perpDistance = rays[x].distance * cos(rays[x].rayAngle - player.rotationAngle);
-		float projectedWallHeight = (TILE_SIZE / perpDistance) * DIST_PROJ_PLANE;
-
-		int wallStripHeight = (int)projectedWallHeight;
-
-		int wallTopPixel = (WINDOW_HEIGHT / 2) - (wallStripHeight / 2);
-		wallTopPixel = wallTopPixel < 0 ? 0 : wallTopPixel;
-
-		int wallBottomPixel = (WINDOW_HEIGHT / 2) + (wallStripHeight / 2);
-		wallBottomPixel = wallBottomPixel > WINDOW_HEIGHT ? WINDOW_HEIGHT : wallBottomPixel;
-
-		// set the color of the ceilling
-		for (int y = 0; y < wallTopPixel; y++) {
-			drawPixel(x, y, 0xFF444444);
-		}
-
-		int textureOffsetX;
-		if (rays[x].wasHitVertical) {
-			textureOffsetX = (int)rays[x].wallHitY % TILE_SIZE;
-		}
-		else {
-			textureOffsetX = (int)rays[x].wallHitX % TILE_SIZE;
-		}
-
-		// get the correct texture id number from the map content
-		int textNum = rays[x].wallHitContent - 1;
-
-		int texture_width = wallTextures[textNum].width;
-		int texture_height = wallTextures[textNum].height;
-
-		// render the wall from wallTopPixel to wallBottomPixel
-		for (int y = wallTopPixel; y < wallBottomPixel; y++) {
-			int distanceFromTop = (y + (wallStripHeight / 2) - (WINDOW_HEIGHT / 2));
-			int textureOffsetY = distanceFromTop * ((float)texture_height / wallStripHeight);
-
-			// set the color of the wall based on the color from the texture
-			uint32_t texelColor = wallTextures[textNum].texture_buffer[(texture_width * textureOffsetY) + textureOffsetX];
-			drawPixel(x, y, texelColor);
-		}
-
-		// set the color of the floor
-		for (int y = wallBottomPixel; y < WINDOW_HEIGHT; y++) {
-			drawPixel(x, y, 0xFF888888);
-		}
-
-	}
-}
-
 void render(void) {
 	clearColorBuffer(0xFF000000);
 
@@ -164,5 +114,5 @@ int main(int argc, char* args[]) {
 
 	releaseResources();
 
-	return 0;
+	return EXIT_SUCCESS;
 }
---
+++ raycasting-c/wall.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <math.h>
+#include "constants.h"
+#include "player.h"
+#include "ray.h"
+#include "graphics.h"
+#include "textures.h"
+
+void renderWallProjection(void);
