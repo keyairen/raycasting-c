3071606bc432141c83614094bd7678990529dbf7
keyairen <179758374+keyairen@users.noreply.github.com>
2025-05-11 13:31:44 -0700

    Refactor player and ray code

---
+++ raycasting-c/ray.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdbool.h>
+#include <float.h>
+#include "constants.h"
+#include "player.h"
+
+typedef struct Ray {
+	float rayAngle;
+	float wallHitX;
+	float wallHitY;
+	float distance;
+	bool wasHitVertical;
+	int wallHitContent;
+} ray_t;
+
+extern ray_t rays[NUM_RAYS];
+
+float normalizeAngle(float angle);
+float distanceBetweenPoints(float x1, float y1, float x2, float y2);
+void castRay(float rayAngle, int stripId);
+void castAllRays(void);
+void renderRays(void);
